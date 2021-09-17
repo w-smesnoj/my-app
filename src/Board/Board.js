06baec87c1e47f8975a6a6d8b97f92f7c4d546ab
someone0815 <hexx.heyy@web.de>
2021-09-17 19:35:42 +0200

    half working highlighted item feature

--- src/Board/Board.js
+++ src/Board/Board.js
@@ -2,32 +2,49 @@ import React from 'react';
 import './Board.css';
 import { items } from './itemConfig.js';
 
-class Shape extends React.Component {
-  render() {
-    const cfg = this.props.config;
-    const shapeStyle = {
-      background: '#febd45',
-      width: cfg.dim.w,
-      height: cfg.dim.h,
-      transform: `translate(${cfg.pos.x}px, ${cfg.pos.y}px) translate(-50%, -50%)`,
-    };
-    return <div style={shapeStyle}></div>;
-  }
-}
+const Shape = React.forwardRef((props, ref) => {
+  const cfg = props.config;
+  const shapeStyle = {
+    background: '#febd45',
+    width: cfg.dim.w,
+    height: cfg.dim.h,
+    transform: `translate(${cfg.pos.x}px, ${cfg.pos.y}px) translate(-50%, -50%)`,
+  };
+  return <div style={shapeStyle} ref={ref} onClick={props.onClick}></div>;
+});
 
-class Connection extends React.Component {
+const Connection = React.forwardRef((props, ref) => {
+  const cfg = props.config;
+  return (
+    <line
+      x1={cfg.from.pos.x}
+      y1={cfg.from.pos.y}
+      x2={cfg.to.pos.x}
+      y2={cfg.to.pos.y}
+      style={{
+        stroke: 'black',
+        strokeWidth: '3px',
+      }}
+      ref={ref}
+      onClick={props.onClick}
+    />
+  );
+});
+
+class ItemEditor extends React.Component {
   render() {
-    const cfg = this.props.config;
+    if (this.props.item.ref === undefined) return <div></div>;
+    const ref = this.props.item.ref.getBoundingClientRect();
+    console.log(ref.x);
     return (
-      <line
-        x1={cfg.from.pos.x}
-        y1={cfg.from.pos.y}
-        x2={cfg.to.pos.x}
-        y2={cfg.to.pos.y}
+      <div
+        className='highlight-container'
         style={{
-          stroke: 'black',
+          width: `${ref.width}px`,
+          height: `${ref.height}px`,
+          transform: `translate(${ref.x}px,${ref.y}px)`,
         }}
-      />
+      ></div>
     );
   }
 }
@@ -37,6 +54,10 @@ export default class Board extends React.Component {
     super(props);
     this.state = {
       items: items,
+      focusedItem: {
+        item: null,
+        txt: null,
+      },
     };
   }
 
@@ -48,22 +69,46 @@ export default class Board extends React.Component {
       items: itemArray,
     });
   }
-
+  focusItem(e, item, ref) {
+    e.stopPropagation();
+    e.nativeEvent.stopImmediatePropagation();
+    this.setState({
+      focusedItem: {
+        item: item,
+        ref: ref.current,
+      },
+    });
+  }
   render() {
     let shapes = [];
     let smartConnections = [];
 
     this.state.items.forEach((item) => {
+      const ref = React.createRef();
       switch (item.type) {
         case 'shape':
-          shapes.push(<Shape config={item} key={item.id} />);
+          shapes.push(
+            <Shape
+              config={item}
+              key={item.id}
+              ref={ref}
+              onClick={(e) => this.focusItem(e, item, ref)}
+            />
+          );
           break;
         case 'smart-connection':
           const from = this.state.items.find((i) => i.id === item.from.id);
           const to = this.state.items.find((i) => i.id === item.to.id);
           item.from.pos = from.pos;
           item.to.pos = to.pos;
-          smartConnections.push(<Connection config={item} key={item.id} />);
+          smartConnections.push(
+            <Connection
+              config={item}
+              key={item.id}
+              ref={ref}
+              onClick={(e) => this.focusItem(e, item, ref)}
+            />
+          );
           break;
         default:
           break;
@@ -72,6 +117,7 @@ export default class Board extends React.Component {
 
     return (
       <div>
+        <ItemEditor item={this.state.focusedItem} />
         <div className='board'>
           {shapes}
           <svg>{smartConnections}</svg>
